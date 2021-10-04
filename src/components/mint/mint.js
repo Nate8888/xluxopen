f09dae11d6885fa207b9a8283279b32b42991a1a
Mauricio <mauriciofigueiredo@knights.ucf.edu>
2021-10-04 15:16:58 -0400

    Added home page

---
+++ src/components/mint/mint.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import './mint.css'
+
+class Mint extends React.Component {
+    render() {
+        return (
+            <div className="mint-container">
+                <div className="mint auto">
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Mint;
