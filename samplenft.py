86564dd4fc19f2915c9544e36d2307201b773cdb
Nate8888 <wilk.nathan@gmail.com>
2021-10-04 23:18:46 -0400

    Create A NFT on the XRPL using proposal fromWietseWind

--- samplenft.py
+++ samplenft.py
@@ -1,105 +1,115 @@
+# Connect ----------------------------------------------------------------------
 import xrpl
+testnet_url = "https://s.altnet.rippletest.net:51234"
+client = xrpl.clients.JsonRpcClient(testnet_url)
 
-# Connect to the xrpl testnet
-xrpl_client = xrpl.Client('wss://test.xrp.xpring.io/sock')
+# Address
+# rEzp7civgsK3X9sEFTr7s2t6uT27NmNSm2
+# Secret
+# sp5KtuzzRteeCCTu6JsurowZWZjEM
+# Balance
+# 1,000 XRP
+# Sequence Number
+# 21580734
 
-# Get the current ledger sequence
-ledger_sequence = xrpl_client.get_ledger_sequence()
-print(ledger_sequence)
-
-# Get the current fee
-fee = xrpl_client.get_current_fee()
-print(fee)
-
-
-# Write a function that mints a NFT in the xrpl testnet
-def mint_nft(address, secret, token_id, token_name, token_desc):
-    # Create a payment transaction
-    payment = xrpl.Payment(
-        destination=address,
-        amount=xrpl.drops_to_xrp(1),
-        destination_tag=xrpl.random_uint32(),
-        currency='XRP'
-    )
-    # Create a trustline transaction
-    trustline = xrpl.TrustSet(
-        account=address,
-        currency='NFT',
-        limit='0',
-        quality_in='1',
-        quality_out='1'
-    )
-    # Create a NFT token
-    nft_token = xrpl.NFToken(
-        token_id=token_id,
-        token_name=token_name,
-        token_desc=token_desc
-    )
-    # Create a transaction that includes the NFT
-    transaction = xrpl.Transaction(
-        source_address=address,
-        sequence=xrpl.random_uint32(),
-        fee=xrpl.drops_to_xrp(1),
-        last_ledger_sequence=ledger_sequence,
-        payment=payment,
-        trustline=trustline,
-        nft_token=nft_token
-    )
-    # Sign the transaction
-    transaction.sign(secret)
-    # Submit the transaction
-    return xrpl_client.submit(transaction)
-
-# Write a function that sends a payment with an NFT in the xrpl testnet
-def send_nft_payment(address, secret, destination, token_id):
-    # Create a payment transaction
-    payment = xrpl.Payment(
-        destination=destination,
-        amount=xrpl.drops_to_xrp(1),
-        destination_tag=xrpl.random_uint32(),
-        currency='XRP'
-    )
-    # Create a transaction that includes the NFT
-    transaction = xrpl.Transaction(
-        source_address=address,
-        sequence=xrpl.random_uint32(),
-        fee=xrpl.drops_to_xrp(1),
-        last_ledger_sequence=ledger_sequence,
-        payment=payment,
-        nft_set_token=xrpl.NFToken(
-            token_id=token_id
-        )
-    )
-    # Sign the transaction
-    transaction.sign(secret)
-    # Submit the transaction
-    return xrpl_client.submit(transaction)
+# Get credentials from the Testnet Faucet --------------------------------------
+# For production, instead create a Wallet instance
+faucet_url = "https://faucet.altnet.rippletest.net/accounts"
+print("Getting 2 new accounts from the Testnet faucet...")
+from xrpl.wallet import generate_faucet_wallet
+cold_wallet = generate_faucet_wallet(client, debug=True)
+hot_wallet = generate_faucet_wallet(client, debug=True)
 
 
-# Mint an NFT
-nft_id = 'NFT' + str(xrpl.random_uint64())
-nft_name = 'Nathan'
-nft_desc = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
-mint_nft(
-    address='rPEPPER7kfTD9w2To4CQkQZBhjuhCpz9qt',
-    secret='ssJcxvGXGn11pT7R3v7y2Xwf3m2iYaY0WVRpVvSJG5EaEqVFBcK',
-    token_id=nft_id,
-    token_name=nft_name,
-    token_desc=nft_desc
+# Configure issuer (cold address) settings -------------------------------------
+cold_settings_tx = xrpl.models.transactions.AccountSet(
+    account=cold_wallet.classic_address,
+    transfer_rate=0,
+    set_flag=xrpl.models.transactions.AccountSetFlag.ASF_DEFAULT_RIPPLE,
 )
 
-# Send the NFT to a different address
-send_nft_payment(
-    address='rPEPPER7kfTD9w2To4CQkQZBhjuhCpz9qt',
-    secret='ssJcxvGXGn11pT7R3v7y2Xwf3m2iYaY0WVRpVvSJG5EaEqVFBcK',
-    destination='r4H3FsyfPcXCZL34iNMY6XqaiXfLz8U4e2Y',
-    token_id=nft_id
+cst_prepared = xrpl.transaction.safe_sign_and_autofill_transaction(
+    transaction=cold_settings_tx,
+    wallet=cold_wallet,
+    client=client,
+)
+print("Sending cold address AccountSet transaction...")
+response = xrpl.transaction.send_reliable_submission(cst_prepared, client)
+print(response)
+
+
+# Configure hot address settings -----------------------------------------------
+hot_settings_tx = xrpl.models.transactions.AccountSet(
+    account=hot_wallet.classic_address,
+    set_flag=xrpl.models.transactions.AccountSetFlag.ASF_REQUIRE_AUTH,
+)
+hst_prepared = xrpl.transaction.safe_sign_and_autofill_transaction(
+    transaction=hot_settings_tx,
+    wallet=hot_wallet,
+    client=client,
+)
+print("Sending hot address AccountSet transaction...")
+response = xrpl.transaction.send_reliable_submission(hst_prepared, client)
+print(response)
+
+
+# Create trust line from hot to cold address -----------------------------------
+currency_code = "64656E6163687477616368740000000000000000"
+trust_set_tx = xrpl.models.transactions.TrustSet(
+    account=hot_wallet.classic_address,
+    limit_amount=xrpl.models.amounts.issued_currency_amount.IssuedCurrencyAmount(
+        currency=currency_code,
+        issuer=cold_wallet.classic_address,
+        value="1000000000000000e-96", # Large limit, arbitrarily chosen
+    )
 )
 
-# Get the NFT
-nft = xrpl_client.get_nft(nft_id)
-print(nft)
+ts_prepared = xrpl.transaction.safe_sign_and_autofill_transaction(
+    transaction=trust_set_tx,
+    wallet=hot_wallet,
+    client=client,
+)
 
-# Get the NFTs owned by a specific address
-nfts = xrpl_client.get_nfts(address='r4H3FsyfPcXCZL34iNMY6XqaiXfLz8U4e2Y')
-print(nfts)
+print("Creating trust line from hot address to issuer...")
+response = xrpl.transaction.send_reliable_submission(ts_prepared, client)
+print(response)
+
+
+# Send token -------------------------------------------------------------------
+issue_quantity = "1000000000000000e-96"
+send_token_tx = xrpl.models.transactions.Payment(
+    account=cold_wallet.classic_address,
+    destination=hot_wallet.classic_address,
+    amount=xrpl.models.amounts.issued_currency_amount.IssuedCurrencyAmount(
+        currency=currency_code,
+        issuer=cold_wallet.classic_address,
+        value=issue_quantity
+    ),
+    memos=[xrpl.models.transactions.Memo(memo_data="72656e74", memo_type="687474703a2f2f6578616d706c652e636f6d2f6d656d6f2f67656e65726963")]
+)
+
+pay_prepared = xrpl.transaction.safe_sign_and_autofill_transaction(
+    transaction=send_token_tx,
+    wallet=cold_wallet,
+    client=client,
+)
+print(f"Sending {issue_quantity} {currency_code} to {hot_wallet.classic_address}...")
+response = xrpl.transaction.send_reliable_submission(pay_prepared, client)
+print(response)
+
+
+# Check balances ---------------------------------------------------------------
+print("Getting hot address balances...")
+response = client.request(xrpl.models.requests.AccountLines(
+    account=hot_wallet.classic_address,
+    ledger_index="validated",
+))
+print(response)
+
+print("Getting cold address balances...")
+response = client.request(xrpl.models.requests.GatewayBalances(
+    account=cold_wallet.classic_address,
+    ledger_index="validated",
+    hotwallet=[hot_wallet.classic_address]
+))
+print(response)
